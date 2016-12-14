cb90aa0f5b5f24682bd5f7e235d2c8b08f749856
Juanjo Cerezuela <pliyosan@gmail.com>
2016-12-14 14:28:39 +0000

    Updating to 0.3.2

--- lib/sensu-plugins-influxdb-metrics-checker/version.rb
+++ lib/sensu-plugins-influxdb-metrics-checker/version.rb
@@ -2,7 +2,7 @@ module SensuPluginsInfluxDbMetricsChecker
   module Version
     MAJOR = 0
     MINOR = 3
-    PATCH = 1
+    PATCH = 2
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
